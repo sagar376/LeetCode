cbfbe22d1acb30d3c157cfe6559c3a30d7744a95
Sagar K <58427458+sagar376@users.noreply.github.com>
2023-10-13 06:47:08 +0530

    Time: 2 ms (65.70%), Space: 50.5 MB (98.26%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -16,21 +16,47 @@ class Solution {
 //         }
 //   return res;    
         
-       int[]  arr =new int[nums.length];
-        int left =1; int right=1;
+    //    int[]  arr =new int[nums.length];
+    //     int left =1; int right=1;
+    //     for(int i=0;i<nums.length;i++)
+    //     {
+    //         arr[i]=left;
+    //         left=left*nums[i];
+    //     }
+    //     System.out.print(Arrays.toString(arr));
+    //     for(int i=nums.length-1;i>=0;i--)
+    //     {
+    //         arr[i]=arr[i]*right;
+    //         right=right*nums[i];
+    //     }
+    //     return arr;
+    // }
+        int[] prefix = new int[nums.length];
+        int[] postfix=new int[nums.length];
+        int left =1 ; int right=1;
+        prefix[0]=left;
+        for(int i=1;i<nums.length;i++)
+        {    
+            left=nums[i-1];  
+             prefix[i]=left*prefix[i-1];
+        }
+        //System.out.println(Arrays.toString(prefix));
+        postfix[nums.length-1]=1;
+        for(int i=nums.length-2;i>=0;i--)
+        {
+            right=nums[i+1];
+            postfix[i]=right*postfix[i+1];
+        }
+        //System.out.println(Arrays.toString(postfix));
         for(int i=0;i<nums.length;i++)
         {
-            arr[i]=left;
-            left=left*nums[i];
+            nums[i]=prefix[i]*postfix[i];
         }
-        System.out.print(Arrays.toString(arr));
-        for(int i=nums.length-1;i>=0;i--)
-        {
-            arr[i]=arr[i]*right;
-            right=right*nums[i];
-        }
-        return arr;
+        
+        return nums;
     }
+    
+        
 }
 // Alternate approach is to multiply values in array and store the product, 
 // on each index divide the whole product with index value.

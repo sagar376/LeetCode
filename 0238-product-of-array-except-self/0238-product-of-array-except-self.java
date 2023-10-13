8f2d18de14ee6b3fdc443abd8715f7eb7de6e339
Sagar K <58427458+sagar376@users.noreply.github.com>
2023-10-13 07:19:59 +0530

    Time: 2 ms (65.70%), Space: 51.8 MB (71.11%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -1,17 +1,62 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        int[] output = new int[nums.length];
-        output[0] = 1;
-
-        for (int i = 0; i < nums.length - 1; i++) output[i + 1] =
-            output[i] * nums[i];
-
-        for (int i = nums.length - 2; i >= 0; i--) {
-            output[i] = nums[i + 1] * output[i];
-            nums[i] = nums[i] * nums[i + 1];
+//         int[] res=new int[nums.length];
+        
+//         for(int loop=0;loop<nums.length;loop++)
+//         {
+//             int product=1;
+//             for(int i=0;i<nums.length;i++)
+//             {
+//                 if(i!=loop)
+//                 {
+//                     product=product*nums[i];
+//                 }
+//             }
+//             res[loop]=product;
+//         }
+//   return res;    
+        
+    //    int[]  arr =new int[nums.length];
+    //     int left =1; int right=1;
+    //     for(int i=0;i<nums.length;i++)
+    //     {
+    //         arr[i]=left;
+    //         left=left*nums[i];
+    //     }
+    //     System.out.print(Arrays.toString(arr));
+    //     for(int i=nums.length-1;i>=0;i--)
+    //     {
+    //         arr[i]=arr[i]*right;
+    //         right=right*nums[i];
+    //     }
+    //     return arr;
+    // }
+        int[] prefix = new int[nums.length];
+        //int[] postfix=new int[nums.length];
+        int left =1 ; int product=1;
+        prefix[0]=left;
+        for(int i=1;i<nums.length;i++)
+        {    
+            left=nums[i-1];  
+             prefix[i]=left*prefix[i-1];
         }
-        return output;
+        //System.out.println(Arrays.toString(prefix));
+        
+        for(int i=nums.length-2;i>=0;i--)
+        {
+            
+             product*=nums[i+1];
+            prefix[i]=prefix[i]*product;
+           
+        }
+        //System.out.println(Arrays.toString(postfix));
+
+        
+        return prefix;
     }
     
         
 }
+// Alternate approach is to multiply values in array and store the product, 
+// on each index divide the whole product with index value.
+// BUT we are given that we should not be using division operation.

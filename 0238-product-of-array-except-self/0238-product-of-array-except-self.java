0bed39d364dbbc79001574f592ae347567a0d5ea
Sagar K <58427458+sagar376@users.noreply.github.com>
2023-10-13 07:17:29 +0530

    Time: 2 ms (65.70%), Space: 53.3 MB (31.02%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -1,36 +1,5 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-//         int[] res=new int[nums.length];
-        
-//         for(int loop=0;loop<nums.length;loop++)
-//         {
-//             int product=1;
-//             for(int i=0;i<nums.length;i++)
-//             {
-//                 if(i!=loop)
-//                 {
-//                     product=product*nums[i];
-//                 }
-//             }
-//             res[loop]=product;
-//         }
-//   return res;    
-        
-    //    int[]  arr =new int[nums.length];
-    //     int left =1; int right=1;
-    //     for(int i=0;i<nums.length;i++)
-    //     {
-    //         arr[i]=left;
-    //         left=left*nums[i];
-    //     }
-    //     System.out.print(Arrays.toString(arr));
-    //     for(int i=nums.length-1;i>=0;i--)
-    //     {
-    //         arr[i]=arr[i]*right;
-    //         right=right*nums[i];
-    //     }
-    //     return arr;
-    // }
         int[] prefix = new int[nums.length];
         int left =1 ; int product=1;
         prefix[0]=left;
@@ -52,6 +21,3 @@ class Solution {
     
         
 }
-// Alternate approach is to multiply values in array and store the product, 
-// on each index divide the whole product with index value.
-// BUT we are given that we should not be using division operation.

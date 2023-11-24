20ba730efcc7ccc6aa06832368def1331e2cbdd1
Sagar K <58427458+sagar376@users.noreply.github.com>
2023-11-24 05:40:40 +0530

    Time: 0 ms (100.00%), Space: 43.4 MB (30.44%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.java
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.java
@@ -16,24 +16,74 @@ class Node {
 class Solution {
     public Node copyRandomList(Node head) {
         
-        Node cur=head;
-        HashMap<Node,Node> hp=new HashMap<>();
+//         Node cur=head;
+//         HashMap<Node,Node> hp=new HashMap<>();
         
-        while(cur!=null)
+//         while(cur!=null)
+//         {
+//             hp.put(cur,new Node(cur.val));
+//             cur=cur.next;
+//         }
+        
+//         cur = head;
+        
+//         while(cur!=null)
+//         {
+//             hp.get(cur).next=hp.get(cur.next);
+//             hp.get(cur).random=hp.get(cur.random);
+//             cur=cur.next;
+//         }
+//         return hp.get(head);
+        Node iter = head;
+        Node front =head;
+        Node copy;
+        
+        //step-1 //insert new copynodes in between
+        while(iter!=null)
         {
-            hp.put(cur,new Node(cur.val));
-            cur=cur.next;
+            front = iter.next;
+            copy=new Node(iter.val);
+            iter.next=copy;
+            copy.next=front;
+            iter = front;
         }
         
-        cur = head;
-        
-        while(cur!=null)
+        //step -2  // add random pointer or copy nodes
+        iter=head;
+        while(iter!=null)
         {
-            hp.get(cur).next=hp.get(cur.next);
-            hp.get(cur).random=hp.get(cur.random);
-            cur=cur.next;
+            if(iter.random!= null)
+            {
+                //iter.next.random=null;
+                iter.next.random=iter.random.next;
+            }
+            else{
+                //iter.next.random=null;
+            }
+            
+            iter=iter.next.next;
         }
-        return hp.get(head);
-      
+        
+        //step - 3 // break the original and copy nodes and connect back copy and original node's link
+        Node dummy=new Node(0);
+        iter=head;
+        front=head;
+        Node copy1=dummy;
+        
+        while(iter!=null && front!=null)
+        {
+            front=iter.next.next;
+            copy1.next=iter.next;
+            iter.next=front;
+            copy1=copy1.next;
+            iter=front;
+        }
+        
+        
+        
+        return dummy.next;
+        
+        
+        
     }
 }

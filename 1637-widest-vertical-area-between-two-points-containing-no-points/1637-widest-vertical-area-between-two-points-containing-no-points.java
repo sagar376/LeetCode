0292fc07b74c5c491a549613032e77f7db616523
Sagar K <58427458+sagar376@users.noreply.github.com>
2023-12-22 01:42:39 +0530

    Time: 48 ms (6.56%), Space: 82 MB (5.02%) - LeetHub

--- 1637-widest-vertical-area-between-two-points-containing-no-points/1637-widest-vertical-area-between-two-points-containing-no-points.java
+++ 1637-widest-vertical-area-between-two-points-containing-no-points/1637-widest-vertical-area-between-two-points-containing-no-points.java
@@ -1,20 +1,40 @@
 class Solution {
     public int maxWidthOfVerticalArea(int[][] points) {
-        int max=0;
+//         int max=0;
+//         int size=points.length;
+//         int[] xAxis=new int[size];
+        
+//         for(int i=0;i<size;i++)
+//         {
+//             xAxis[i]=points[i][0];
+//         }
+//         Arrays.sort(xAxis);
+//         //System.out.print(Arrays.toString(xAxis));
+        
+//         for(int i=size-1;i>0;i--)
+//         {
+//             max=Math.max(max,(xAxis[i]-xAxis[i-1]));
+//         }
+//         return max;
+        
+         Set<Integer> xCoordinates= new TreeSet<>();
         int size=points.length;
-        int[] xAxis=new int[size];
         
         for(int i=0;i<size;i++)
         {
-            xAxis[i]=points[i][0];
+            xCoordinates.add(points[i][0]);
         }
-        Arrays.sort(xAxis);
-        //System.out.print(Arrays.toString(xAxis));
-        
-        for(int i=size-1;i>0;i--)
+        int maxWidth=0;
+        Integer prevX=null;
+        for(int x:xCoordinates)
         {
-            max=Math.max(max,(xAxis[i]-xAxis[i-1]));
+            if(prevX!=null)
+            {
+                maxWidth=Math.max(maxWidth,x-prevX);
+            }
+            prevX=x;
+            
         }
-        return max;
+        return maxWidth;
     }
 }

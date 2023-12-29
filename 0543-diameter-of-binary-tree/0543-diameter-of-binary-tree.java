d865ff80396c8d2e72662863ff6bb45d51192036
Sagar K <58427458+sagar376@users.noreply.github.com>
2023-12-30 01:29:37 +0530

    Time: 1 ms (19.76%), Space: 42.4 MB (27.52%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
@@ -29,9 +29,16 @@ class Solution {
             return -1;
         }
         
-        int left=1+dfs(current.left);
-        int right=1+dfs(current.right);
-        result=Math.max(result,(left+right));
-        return Math.max(left,right);
+        // int left=dfs(current.left);
+        // int right=dfs(current.right);
+        // result=Math.max(result,(left+right+2));
+        // return 1+Math.max(left,right);
+
+        //OR 
+
+        int left = 1 + dfs(current.left);
+        int right = 1 + dfs(current.right);
+        result = Math.max(result, (left + right));
+        return Math.max(left, right);
     }
 }

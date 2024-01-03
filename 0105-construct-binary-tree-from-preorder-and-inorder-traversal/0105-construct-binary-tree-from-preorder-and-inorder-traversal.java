b9147b3cd6b851cb5a0521c23cdc48565610a435
Sagar K <58427458+sagar376@users.noreply.github.com>
2024-01-03 17:20:00 +0530

    Time: 1 ms (93.81%), Space: 44.2 MB (27.05%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -13,25 +13,54 @@
  *     }
  * }
  */
+//Solution-1
 class Solution {
     public TreeNode buildTree(int[] preorder, int[] inorder) {
-        
-        if(preorder.length==0 || inorder.length==0)return null;
-        
-        int mid=0;
-        for(int i=0;i<preorder.length;i++){
-            if(preorder[0]==inorder[i])
-            {
-                mid =i;
-            }
-        }
-        
-        TreeNode root=new TreeNode(preorder[0]);
-        root.left=buildTree(Arrays.copyOfRange(preorder,1,mid+1),Arrays.copyOfRange(inorder,0,mid));
-        root.right=buildTree(Arrays.copyOfRange(preorder,mid+1,preorder.length),Arrays.copyOfRange(inorder,mid+1,inorder.length));
-        
-        return root;
-        
-        
+    Map<Integer, Integer> inMap = new HashMap<Integer, Integer>();
+
+    for(int i = 0; i < inorder.length; i++) {
+        inMap.put(inorder[i], i);
     }
+
+    TreeNode root = buildTree(preorder, 0, preorder.length - 1, inorder, 0, inorder.length - 1, inMap);
+    return root;
 }
+
+public TreeNode buildTree(int[] preorder, int preStart, int preEnd, int[] inorder, int inStart, int inEnd, Map<Integer, Integer> inMap) {
+    if(preStart > preEnd || inStart > inEnd) return null;
+
+    TreeNode root = new TreeNode(preorder[preStart]);
+    int inRoot = inMap.get(root.val);
+    int numsLeft = inRoot - inStart;
+
+    root.left = buildTree(preorder, preStart + 1, preStart + numsLeft, inorder, inStart, inRoot - 1, inMap);
+    root.right = buildTree(preorder, preStart + numsLeft + 1, preEnd, inorder, inRoot + 1, inEnd, inMap);
+
+    return root;
+}
+}
+
+
+//Solution-2
+// class Solution {
+//     public TreeNode buildTree(int[] preorder, int[] inorder) {
+        
+//         if(preorder.length==0 || inorder.length==0)return null;
+        
+//         int mid=0;
+//         for(int i=0;i<preorder.length;i++){
+//             if(preorder[0]==inorder[i])
+//             {
+//                 mid =i;
+//             }
+//         }
+        
+//         TreeNode root=new TreeNode(preorder[0]);
+//         root.left=buildTree(Arrays.copyOfRange(preorder,1,mid+1),Arrays.copyOfRange(inorder,0,mid));
+//         root.right=buildTree(Arrays.copyOfRange(preorder,mid+1,preorder.length),Arrays.copyOfRange(inorder,mid+1,inorder.length));
+        
+//         return root;
+        
+        
+//     }
+// }

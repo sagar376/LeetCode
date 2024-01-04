ffb679fcddf8b8ffe82e9790e22e72bc1416a3c5
Sagar K <58427458+sagar376@users.noreply.github.com>
2024-01-04 06:38:28 +0530

    Time: 13 ms (59.77%), Space: 45.2 MB (17.22%) - LeetHub

--- 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.java
+++ 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.java
@@ -1,25 +1,47 @@
+// class Solution {
+//     public int numberOfBeams(String[] bank) {
+//         int size=bank.length;
+//         int res=0;
+//         List<Integer> floorList=new ArrayList<>();
+        
+//         for(String floor:bank)
+//         {   
+//             int count=0;
+//             for(int i=0;i<floor.length();i++)
+//             {
+//                 if(floor.charAt(i)=='1')
+//                     count++;
+//             }
+//             if(count>0)
+//             floorList.add(count); 
+//         }
+        
+//         for(int i=0;i<floorList.size()-1;i++)
+//         {
+//             res+=floorList.get(i)*floorList.get(i+1);
+//         }
+//         return res;
+//     }
+// }
+
+//solution -2 
 class Solution {
-    public int numberOfBeams(String[] bank) {
-        int size=bank.length;
-        int res=0;
-        List<Integer> floorList=new ArrayList<>();
-        
-        for(String floor:bank)
-        {   
-            int count=0;
-            for(int i=0;i<floor.length();i++)
-            {
-                if(floor.charAt(i)=='1')
-                    count++;
-            }
-            if(count>0)
-            floorList.add(count); 
+  public int numberOfBeams(String[] bank) {
+    int prev = 0, ans = 0;
+
+    for (String s: bank) {
+      int count = 0;
+      for (int i = 0; i < s.length(); i++)
+        if (s.charAt(i) == '1') {
+          count++;
         }
-        
-        for(int i=0;i<floorList.size()-1;i++)
-        {
-            res+=floorList.get(i)*floorList.get(i+1);
-        }
-        return res;
+
+      if (count > 0) {
+        ans += prev * count;
+        prev = count;
+      }
     }
+
+    return ans;
+  }
 }

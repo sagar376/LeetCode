ae5214a9188f5740e251e7fbd07b9e3043c55591
Sagar K <58427458+sagar376@users.noreply.github.com>
2024-02-03 00:20:16 +0530

    Time: 0 ms (100.00%), Space: 40.9 MB (42.41%) - LeetHub

--- 1291-sequential-digits/1291-sequential-digits.java
+++ 1291-sequential-digits/1291-sequential-digits.java
@@ -1,16 +1,37 @@
 class Solution {
     public List<Integer> sequentialDigits(int low, int high) {
         
-        String possible="123456789";
         List<Integer> ans=new ArrayList<>();
         
-        for(int i=0;i<possible.length()-1;i++)
-        for(int j=i+1;j<possible.length();j++)
+        for(int i=1;i<=9;i++)
         {
-            int num=Integer.parseInt(possible.substring(i,j+1));
-            if(num >=low && num <= high)ans.add(num);
+            int num=i;
+            int next=i+1;
+            while(num <=high && next<=9)
+            {
+                num=num*10+next;
+                if(low<=num && num <= high)
+                {
+                    ans.add(num);
+                }
+                next++;
+            }
+            ans.sort(null);
         }
-        ans.sort(null);
         return ans;
+        
+        
+        //Method-2
+//         String possible="123456789";
+//         List<Integer> ans=new ArrayList<>();
+        
+//         for(int i=0;i<possible.length()-1;i++)
+//         for(int j=i+1;j<possible.length();j++)
+//         {
+//             int num=Integer.parseInt(possible.substring(i,j+1));
+//             if(num >=low && num <= high)ans.add(num);
+//         }
+//         ans.sort(null); // important method
+//         return ans;
     }
 }

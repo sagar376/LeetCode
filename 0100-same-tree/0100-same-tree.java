223a8712ff7d241c22a88afef209f0e51e1ee862
Sagar K <58427458+sagar376@users.noreply.github.com>
2024-02-26 14:40:18 +0530

    Time: 0 ms (100%), Space: 40.7 MB (82.26%) - LeetHub

--- 0100-same-tree/0100-same-tree.java
+++ 0100-same-tree/0100-same-tree.java
@@ -39,3 +39,12 @@ class Solution {
 
     }
 }
+
+// Solution -2 
+// class Solution {
+//     public boolean isSameTree(TreeNode p, TreeNode q) {
+//         if (p == null && q == null) return true;
+//         if (p == null || q == null) return false;
+//         return p.val == q.val && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+//     }
+// }

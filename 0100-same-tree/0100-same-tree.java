4a4543047e86c4c153ece6ba86a7359ab0fe220c
Sagar K <58427458+sagar376@users.noreply.github.com>
2024-02-26 14:37:21 +0530

    Time: 0 ms (100%), Space: 40.9 MB (54.87%) - LeetHub

--- 0100-same-tree/0100-same-tree.java
+++ 0100-same-tree/0100-same-tree.java
@@ -15,26 +15,27 @@
  */
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        
-       return dfs(p,q);
-        
+
+        return dfs(p,q); 
     }
-    
-    public boolean dfs(TreeNode p,TreeNode q)
+
+    private boolean dfs(TreeNode p,TreeNode q)
     {
-        if(p==null && q==null)
-        {
-            return true;
-        }
-        if(p==null || q==null)
+        if(p == null && q== null)
+        return true;
+
+        if(p == null || q == null)
         {
             return false;
         }
-        if(p.val!=q.val)return false;
-        
+
+        if(p.val != q.val)
+        return false;
+
         boolean left=dfs(p.left,q.left);
         boolean right=dfs(p.right,q.right);
-        
+
         return left && right;
+
     }
 }

bf56fdeed91ecb8239644b52cbfb63e26db290fb
Sagar K <58427458+sagar376@users.noreply.github.com>
2024-02-27 15:18:34 +0530

    Time: 0 ms (100%), Space: 42.4 MB (93.13%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
@@ -14,7 +14,7 @@
  * }
  */
 class Solution {
-    int result=-1;
+   int result=-1;
     public int diameterOfBinaryTree(TreeNode root) {
         
         dfs(root);
@@ -29,16 +29,9 @@ class Solution {
             return -1;
         }
         
-        int left=dfs(current.left);
-        int right=dfs(current.right);
-        result=Math.max(result,(left+right+2));
-        return 1+Math.max(left,right);
-
-        //OR  above is NeetCode soln
-
-        // int left = 1 + dfs(current.left);
-        // int right = 1 + dfs(current.right);
-        // result = Math.max(result, (left + right));
-        // return Math.max(left, right);
+        int left=1+dfs(current.left);
+        int right=1+dfs(current.right);
+        result=Math.max(result,(left+right));
+        return Math.max(left,right);
     }
 }

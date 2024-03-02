bdb89ff81ad08f24f581347dc53e7bb96c2726e5
Sagar K <58427458+sagar376@users.noreply.github.com>
2024-03-03 01:31:44 +0530

    Time: 1 ms (100%), Space: 47 MB (54.77%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.java
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.java
@@ -6,18 +6,33 @@ class Solution {
 
         int[] answer=new int[nums.length];
         int start=0,end=nums.length-1;
-        for(int i=nums.length-1;i>=0;i--)
+        // for(int i=nums.length-1;i>=0;i--)
+        // {
+        //     if(Math.abs(nums[start])>=Math.abs(nums[end]))
+        //     {
+        //     answer[i]=nums[start]*nums[start];
+        //     start++;
+        //     }
+        //     else
+        //     {
+        //         answer[i]=nums[end]*nums[end];
+        //         end--;
+        //     }
+        // }
+        int i=nums.length-1;
+        while(start<=end)
         {
             if(Math.abs(nums[start])>=Math.abs(nums[end]))
             {
-            answer[i]=nums[start]*nums[start];
-            start++;
+                answer[i]=nums[start]*nums[start];
+                start++;
             }
             else
             {
                 answer[i]=nums[end]*nums[end];
                 end--;
             }
+            i--;
         }
         return answer;
     }

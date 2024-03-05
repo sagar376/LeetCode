09d1997326073913bb759e9afa71c1016986d0be
Sagar K <58427458+sagar376@users.noreply.github.com>
2024-03-06 04:36:57 +0530

    Time: 0 ms (100%), Space: 41.3 MB (14.96%) - LeetHub

---
+++ 0441-arranging-coins/0441-arranging-coins.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int arrangeCoins(int n) {
+       //Important 3 solutions are possible for same problem
+
+       return (int) (Math.sqrt((long)2*n+0.25)-0.5);
+        
+    }
+}

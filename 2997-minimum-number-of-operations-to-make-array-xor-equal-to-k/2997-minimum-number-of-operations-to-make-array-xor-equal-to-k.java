e05869aa8c826c98dc7ec5a90d322008d33c9017
Sagar K <58427458+sagar376@users.noreply.github.com>
2024-06-07 03:53:39 +0530

    Time: 1 ms (100%), Space: 56.5 MB (54.83%) - LeetHub

--- 2997-minimum-number-of-operations-to-make-array-xor-equal-to-k/2997-minimum-number-of-operations-to-make-array-xor-equal-to-k.java
+++ 2997-minimum-number-of-operations-to-make-array-xor-equal-to-k/2997-minimum-number-of-operations-to-make-array-xor-equal-to-k.java
@@ -7,7 +7,26 @@ class Solution {
             finalXor^=num;
         }
 
-        return Integer.bitCount(finalXor^k);
+    //method-1
+        //return Integer.bitCount(finalXor^k);
+        ///
+
+        //method-2
+        int count=0;
+
+        while(k>0 || finalXor>0)
+        {
+            if( (k%2) != (finalXor%2) )
+            {
+                count++;
+            }
+
+            k/=2;
+            finalXor/=2;
+
+        }
+
+        return count;
         
     }
 }

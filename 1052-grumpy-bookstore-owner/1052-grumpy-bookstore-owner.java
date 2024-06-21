1fc5545acf84c8e4682346aef9615bcfd21052da
Sagar K <58427458+sagar376@users.noreply.github.com>
2024-06-22 01:12:09 +0530

    Time: 3 ms (91.51%), Space: 45.5 MB (99.48%) - LeetHub

---
+++ 1052-grumpy-bookstore-owner/1052-grumpy-bookstore-owner.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int maxSatisfied(int[] customers, int[] grumpy, int minutes) {
+
+        int slidingWindow = minutes;
+        int maxSatisfied=0;
+        for(int i=0;i<customers.length;i++)
+        {
+            maxSatisfied+=grumpy[i]==0?customers[i]:0;
+        }
+        
+        int secretTechnique=0;
+        for(int i=0;i<minutes;i++){
+            if(grumpy[i]==1)
+            {
+                secretTechnique+=customers[i];
+            }
+        }
+
+        int currentMax=secretTechnique;
+        for(int i=minutes;i<customers.length;i++)
+        {
+            if(grumpy[i]==1 && grumpy[i-minutes] ==1)
+            {
+                secretTechnique-=customers[i-minutes];
+                secretTechnique+=customers[i];
+            }
+            else if(grumpy[i]==0 && grumpy[i-minutes] ==1)
+            {
+                secretTechnique-=customers[i-minutes];
+            }
+            else if(grumpy[i]==1 && grumpy[i-minutes] ==0)
+            {
+                secretTechnique+=customers[i];
+            }
+            currentMax=Math.max(currentMax,secretTechnique);
+        }
+        return currentMax+maxSatisfied;
+        
+    }
+}

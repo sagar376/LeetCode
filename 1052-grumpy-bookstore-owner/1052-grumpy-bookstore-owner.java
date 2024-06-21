5368c7e165f6c92dcb0131c67c95f67fe1adbfdf
Sagar K <58427458+sagar376@users.noreply.github.com>
2024-06-22 01:23:20 +0530

    Time: 3 ms (91.51%), Space: 47.5 MB (33.45%) - LeetHub

--- 1052-grumpy-bookstore-owner/1052-grumpy-bookstore-owner.java
+++ 1052-grumpy-bookstore-owner/1052-grumpy-bookstore-owner.java
@@ -8,7 +8,9 @@ class Solution {
             maxSatisfied+=grumpy[i]==0?customers[i]:0;
         }
         
-        int secretTechnique=0;
+        // int secretTechnique=0;
+
+        int secretTechnique=maxSatisfied;
         for(int i=0;i<minutes;i++){
             if(grumpy[i]==1)
             {
@@ -19,22 +21,28 @@ class Solution {
         int currentMax=secretTechnique;
         for(int i=minutes;i<customers.length;i++)
         {
-            if(grumpy[i]==1 && grumpy[i-minutes] ==1)
-            {
-                secretTechnique-=customers[i-minutes];
-                secretTechnique+=customers[i];
-            }
-            else if(grumpy[i]==0 && grumpy[i-minutes] ==1)
-            {
-                secretTechnique-=customers[i-minutes];
-            }
-            else if(grumpy[i]==1 && grumpy[i-minutes] ==0)
-            {
-                secretTechnique+=customers[i];
-            }
+            // if(grumpy[i]==1 && grumpy[i-minutes] ==1)
+            // {
+            //     secretTechnique-=customers[i-minutes];
+            //     secretTechnique+=customers[i];
+            // }
+            // else if(grumpy[i]==0 && grumpy[i-minutes] ==1)
+            // {
+            //     secretTechnique-=customers[i-minutes];
+            // }
+            // else if(grumpy[i]==1 && grumpy[i-minutes] ==0)
+            // {
+            //     secretTechnique+=customers[i];
+            // }
+
+            //OR  make use of Binary Array
+            secretTechnique-=customers[i-minutes]*grumpy[i-minutes];
+            secretTechnique+=customers[i]*grumpy[i];
+
             currentMax=Math.max(currentMax,secretTechnique);
         }
-        return currentMax+maxSatisfied;
+        // return currentMax+maxSatisfied;
+        return currentMax;
         
     }
 }

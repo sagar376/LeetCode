777553338aeb93ed2a4afb0f91cb13e75c77f4cb
Sagar K <58427458+sagar376@users.noreply.github.com>
2024-06-29 00:06:30 +0530

    Time: 31 ms (30.27%), Space: 79.7 MB (5.41%) - LeetHub

--- 2285-maximum-total-importance-of-roads/2285-maximum-total-importance-of-roads.java
+++ 2285-maximum-total-importance-of-roads/2285-maximum-total-importance-of-roads.java
@@ -1,31 +1,63 @@
+//My Soln
 class Solution {
     public long maximumImportance(int n, int[][] roads) {
-        int row=roads.length;
-        int col=roads[0].length;
-        int[] degree=new int[n];
+        long res = 0;
 
-        for(int i=0;i<row;i++)
-        {
-            for(int j=0;j<col;j++)
-            {
-                degree[roads[i][j]]++;
-            }
+        int [] a = new int[n];
+
+        for(int [] r: roads) {
+            a[r[0]]++;
+            a[r[1]]++;
         }
 
-        Arrays.sort(degree);
+        int [] arr = new int[n + 1];
 
-        long totalImportance=0;
-        int value=1;
+        PriorityQueue<Pair<Integer,Integer>> pq = new PriorityQueue<>((a1, b) -> b.getValue() - a1.getValue());
 
-        for(int num:degree)
-        {
-            totalImportance+=(long)num*value;
-            value++;
+        for(int i = 0; i < n; i++) {
+            pq.add(new Pair<>(i, a[i]));
         }
 
-        return totalImportance ;
+        while(!pq.isEmpty()) {
+            arr[pq.peek().getKey()] = n--;
+            pq.remove();
+        }
+
+        for(int [] r : roads) {
+            res += arr[r[0]] + arr[r[1]];
+        }
+
+        return res;
+    }
+}
+// class Solution {
+//     public long maximumImportance(int n, int[][] roads) {
+//         int row=roads.length;
+//         int col=roads[0].length;
+//         int[] degree=new int[n];
+
+//         for(int i=0;i<row;i++)
+//         {
+//             for(int j=0;j<col;j++)
+//             {
+//                 degree[roads[i][j]]++;
+//             }
+//         }
+
+//         Arrays.sort(degree);
+
+//         long totalImportance=0;
+//         int value=1;
+
+//         for(int num:degree)
+//         {
+//             totalImportance+=(long)num*value;
+//             value++;
+//         }
+
+//         return totalImportance ;
 
 
         
-    }
-}
+//     }
+// }

c43a9ddebb703ae5cbeeda6a5ec588ee30db62cb
Sagar K <58427458+sagar376@users.noreply.github.com>
2024-07-23 01:12:04 +0530

    Time: 8 ms (62.32%), Space: 45.2 MB (50.59%) - LeetHub

---
+++ 2418-sort-the-people/2418-sort-the-people.java
@@ -0,0 +1,34 @@
+class Solution {
+
+    class Person{
+        String name;
+        int height;
+        Person(String name,int height)
+        {
+            this.name = name;
+            this.height = height;
+        }
+    }
+    public String[] sortPeople(String[] names, int[] heights) {
+        
+        Person[] person=new Person[names.length];
+        for(int i=0;i < names.length ; i++)
+        {
+            person[i]=new Person(names[i],heights[i]);
+        }
+
+        PriorityQueue<Person> pq = new PriorityQueue<>((a,b)-> b.height-a.height);
+
+        pq.addAll(Arrays.asList(person));
+
+        String[] result = new String[names.length];
+        int index = 0;
+        while(!pq.isEmpty())
+        {
+            Person curr = pq.poll();
+            result[index++] = curr.name;
+        }
+
+        return result;
+    }
+}

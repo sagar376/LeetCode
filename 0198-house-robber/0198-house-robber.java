b0f46452a3ea73685321f6efab3bf0e73bc0cb9f
Sagar K <58427458+sagar376@users.noreply.github.com>
2024-07-26 03:06:58 +0530

    Time: 0 ms (100%), Space: 41 MB (36.97%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,14 +1,34 @@
 class Solution {
     public int rob(int[] nums) {
-        int rob1=0,rob2=0;
 
-        int temp=0;
-        for(int i:nums)
+
+        int size = nums.length;
+        int[] dp = new int[size];
+
+        Arrays.fill(dp, -1);
+
+        dp[size-1] = solve(size-1, dp, nums);
+
+        return dp[size-1];
+        
+    }
+
+    private int solve(int idx, int[] dp,int[] nums)
+    {
+        if(idx == 0)
+        return nums[0];
+
+        if(idx < 0)
         {
-          temp=Math.max(i+rob1,rob2);
-          rob1=rob2;
-          rob2=temp;
+            return 0;
         }
-        return rob2;
+
+        if(dp[idx] != -1)
+        return dp[idx];
+
+        int pick = nums[idx] + solve(idx-2, dp, nums);
+        int notPick = 0 + solve(idx-1, dp, nums);
+
+        return dp[idx] = Math.max(pick, notPick);
     }
 }

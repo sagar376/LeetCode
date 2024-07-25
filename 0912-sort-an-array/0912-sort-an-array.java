acdf60fa5d6f63b4dfdd1d0629e3ce3113d64634
Sagar K <58427458+sagar376@users.noreply.github.com>
2024-07-26 01:02:10 +0530

    Time: 4 ms (98.1%), Space: 57 MB (20.11%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.java
+++ 0912-sort-an-array/0912-sort-an-array.java
@@ -1,21 +1,52 @@
 class Solution {
     public int[] sortArray(int[] nums) {
 
-        PriorityQueue<Integer> pq=new PriorityQueue<>((a,b) -> a-b);
+        // PriorityQueue<Integer> pq=new PriorityQueue<>((a,b) -> a-b);
 
-        for(int i=0;i<nums.length;i++)
-        {
-            pq.add(nums[i]);
-        }
+        // for(int i=0;i<nums.length;i++)
+        // {
+        //     pq.add(nums[i]);
+        // }
 
-        int result[] = new int[nums.length];
-        int idx = 0;
+        // int result[] = new int[nums.length];
+        // int idx = 0;
 
-        while(!pq.isEmpty())
-        {
-            result[idx++] = pq.poll();
-        }
+        // while(!pq.isEmpty())
+        // {
+        //     result[idx++] = pq.poll();
+        // }
         
-        return result;
+        // return result;
+
+        int min= Integer.MAX_VALUE;
+        int max= Integer.MIN_VALUE;
+
+        for(int num: nums)
+        {
+            min = Math.min(num,min);
+            max = Math.max(num,max);
+        }
+
+        int n = max - min ;
+        int count[] = new int[n+1];
+
+        for(int num: nums)
+        {
+            count[num - min]++;
+        }
+
+        int index =0 ;
+        for(int i=0;i<=n; i++)
+        {
+            while(count[i]>0)
+            {
+                nums[index] = min;
+                index++;
+                count[i]--;
+            }
+            min++;
+        }
+
+        return nums;
     }
 }

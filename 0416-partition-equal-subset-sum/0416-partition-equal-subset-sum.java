86761a4deb40b2f331d0fde4719dcaacecef55ef
Sagar K <58427458+sagar376@users.noreply.github.com>
2024-07-30 09:44:21 +0530

    Time: 81 ms (31.07%), Space: 56.4 MB (38.82%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,49 +1,70 @@
 class Solution {
-    public boolean canPartition(int[] nums) {
 
+    public boolean canPartition(int[] nums) {
         int totSum = 0;
 
-        for(int i = 0; i < nums.length; i++)
-        {
+        for (int i = 0; i < nums.length; i++) {
             totSum += nums[i];
         }
 
-        if(totSum % 2 == 1)
-        return false;
+        if (totSum % 2 == 1) return false;
 
-        int k = totSum/2;
-        boolean[] prev = new boolean[k + 1];
-        // boolean[] curr = new boolean[k + 1];
+        int k = totSum / 2;
+
+
+        int[][] dp =new int[nums.length][k+1];
+
+        for(int[] row:dp)
+        Arrays.fill(row, -1);
+
+        int result = find(nums.length-1, k, nums, dp);
+
+        return dp[nums.length-1][k] == 1;
 
         
 
-        prev[0] = true;
-        // curr[0] = true;
 
-        if(nums[0] == k) prev[nums[0]] = true;
+        // boolean[] prev = new boolean[k + 1];
+        // // boolean[] curr = new boolean[k + 1];
 
-        for(int ind = 1; ind < nums.length; ind++)
-        {
-            boolean[] curr = new boolean[k + 1];
-            curr[0] = true;
-            for(int target = 1 ; target <= k ; target++)
-            {
-                boolean notTake = prev[target];
-                boolean take = false;
-                if(nums[ind] <= target)
-                take = prev[target - nums[ind]];
-                curr[target] = take || notTake;
+        // prev[0] = true;
+        // if (nums[0] == k) prev[nums[0]] = true;
 
-                //System.out.println(Arrays.toString(prev));
-                
-            }
-            // prev = Arrays.copyOf(curr, curr.length);
-            prev = curr;
-        }
+        // System.out.println(Arrays.toString(prev));
 
-        return prev[k];  
-        
+        // for (int ind = 1; ind < nums.length; ind++) {
+        //     boolean[] curr = new boolean[k + 1];
+        //     curr[0] = true;
+        //     for (int target = 1; target <= k; target++) {
+        //         boolean notTake = prev[target];
+        //         boolean take = false;
+        //         if (nums[ind] <= target) take = prev[target - nums[ind]];
+        //         curr[target] = take || notTake;
+        //     }
+            
+        //     prev = curr;
+        //     System.out.println(Arrays.toString(prev));
+        // }
 
+        // return prev[k];
+    }
 
+    private int find(int ind,int target,int[] nums,int[][] dp)
+    {
+        if(target == 0)
+        return 1;
+
+        if(ind == 0)
+        return nums[0] == target?1:0;
+
+        if(dp[ind][target] != -1)
+        return dp[ind][target]; 
+
+        int nottake = find(ind-1, target, nums, dp);
+        int take = 0;
+        if(target >= nums[ind])
+        take = find(ind-1, target - nums[ind] , nums, dp);
+
+        return dp[ind][target] = (nottake + take) >=1 ? 1 :0;
     }
 }

253c3e1e7e83db23d6fd842738fbf3a8056164ae
Sagar K <58427458+sagar376@users.noreply.github.com>
2024-07-30 07:09:37 +0530

    Time: 33 ms (73.43%), Space: 45 MB (77.94%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,43 +1,46 @@
 class Solution {
     public boolean canPartition(int[] nums) {
-      int sum=Arrays.stream(nums).sum();
-      if(sum%2!=0)
-      return false;
-      int n=nums.length;
-      int m=sum/2;
-      boolean[][] dp=new boolean[n+1][m+1];
 
-      for(int i=0;i<n+1;i++)
-      {
-        for(int j=0;j<m+1;j++)
-        {
-          if(i==0)
-          {
-            dp[i][j]=false;
-          }
-          if(j==0)
-          {
-            dp[i][j]=true;
-          }
-        }
-      }
+        int totSum = 0;
 
-    
-      for(int i=1;i<n+1;i++)
-      {
-        for(int j=1;j<m+1;j++)
+        for(int i = 0; i < nums.length; i++)
         {
-          if(nums[i-1]<=j)
-          {
-            dp[i][j]=dp[i-1][j-nums[i-1]] || dp[i-1][j];
-          }
-          else
-          {
-            dp[i][j]=dp[i-1][j];
-          }
+            totSum += nums[i];
         }
-      }
-      return dp[n][m];
+
+        if(totSum % 2 == 1)
+        return false;
+
+        int k = totSum/2;
+        boolean[] prev = new boolean[k + 1];
+        boolean[] curr = new boolean[k + 1];
+
         
+
+        prev[0] = true;
+        curr[0] = true;
+
+        if(nums[0] == k) prev[nums[0]] = true;
+
+        for(int ind = 1; ind < nums.length; ind++)
+        {
+            for(int target = 1 ; target <= k ; target++)
+            {
+                boolean notTake = prev[target];
+                boolean take = false;
+                if(nums[ind] <= target)
+                take = prev[target - nums[ind]];
+                curr[target] = take || notTake;
+
+                //System.out.println(Arrays.toString(prev));
+                
+            }
+            prev = Arrays.copyOf(curr, curr.length);
+        }
+
+        return prev[k];  
+        
+
+
     }
 }

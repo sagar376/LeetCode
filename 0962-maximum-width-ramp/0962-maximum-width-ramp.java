21a52a5ce5ac4d6c47eb79ffbff3c7b12e008d0e
Sagar K <58427458+sagar376@users.noreply.github.com>
2024-10-11 03:27:19 +0530

    Time: 4 ms (89.57%), Space: 54.8 MB (43.85%) - LeetHub

---
+++ 0962-maximum-width-ramp/0962-maximum-width-ramp.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int maxWidthRamp(int[] nums) {
+
+        int n = nums.length;
+        int[] rightMax = new int[n];
+
+        rightMax[n - 1] = nums[n - 1];
+        for(int i = n - 2; i >= 0 ; i--)
+        {
+            rightMax[i] = Math.max(rightMax[i + 1], nums[i]);
+        }
+
+        int left = 0;
+        int right = 0;
+        int maxWidth = 0;
+
+        while(right < n)
+        {
+            while( left < right && nums[left] > rightMax[right])
+            {
+            left ++;
+            }
+            maxWidth = Math.max(maxWidth, right - left);
+            right++;
+        }
+
+        return maxWidth;
+        
+    }
+}
